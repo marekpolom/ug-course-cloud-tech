4f952287a595fe4d331fd4c0c29cd5fdcde693a0
Marek Połom <marekpolom22@gmail.com>
2021-04-06 18:13:35 +0200

    lab05 zadanie 2 modyfikacja serwera

--- lab05/zad02/backend/server.js
+++ lab05/zad02/backend/server.js
@@ -4,12 +4,28 @@ const express = require('express');
 
 const app = express();
 
+const Redis = require("ioredis");
+
 const PORT = process.env.PGPORT;
+
+const dbConnDataRedis = {
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_URL,
+};
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, () => {
-  console.log('Listening on port 5000');
+const clientRedis = new Redis(dbConnDataRedis);
+
+clientRedis.on("error", (err) => {
+console.error("Error connecting to Redis", err);
+});
+
+clientRedis.on("connect", () => {
+    console.log(`Connected to Redis.`);
+    app.listen(PORT, () => {
+        console.log(`API server listening at http://localhost:${PORT}`);
+    });
 });
