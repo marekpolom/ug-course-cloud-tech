c1ee93dd168d08c28d4fd624646d07d022f60011
Marek Połom <marekpolom22@gmail.com>
2021-04-07 15:40:01 +0200

    lab06 zadanie 1 init

---
+++ lab06/zad01/backend/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const Redis = require("ioredis");
+
+const PORT = process.env.PGPORT;
+
+const dbConnDataRedis = {
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_URL,
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const clientRedis = new Redis(dbConnDataRedis);
+
+clientRedis.on("error", (err) => {
+console.error("Error connecting to Redis", err);
+});
+
+clientRedis.on("connect", () => {
+    console.log(`Connected to Redis.`);
+    app.listen(PORT, () => {
+        console.log(`API server listening at http://localhost:${PORT}`);
+    });
+});
